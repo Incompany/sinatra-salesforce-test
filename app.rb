6de2b8995e2da4166943a328f0019fe01fb7a5e2
Luis <lquirosr@gmail.com>
2011-03-28 18:29:36 -0600

    forms de afd y alt listas en heroku

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'rforce'
 
 
 
-    get '/' do
+  get '/' do
 
         haml :contact
     end
@@ -39,7 +39,52 @@ require 'rforce'
                     :Apellidos__c,      "#{lname}",
                     :Correo_Electronico__c,      "#{email}",
                     :Telefono__c,      "#{phone}",
-                    :Descripcion__c,      "#{aux}"
+                    :Descripcion__c,      "#{aux}",
+                    :Empresa_c__c, "AFD Costa Rica"
+                   ]
+
+     			binding.create :sObject => cliente		      
+
+		      haml :exito
+		  end
+
+    get '/alt' do
+
+        haml :contact
+    end
+    
+    
+		post '/alt' do
+		     # name = params[:name]
+		      name = params[:name]
+		      lname = params[:lname]
+		      email= params[:email]
+		      phone = params[:phone]
+		      aux = params[:aux]
+		      
+		      
+		      
+		      puts "nombre:" + "#{name}"
+		      puts "apellido:" + "#{lname}"
+		      puts "email:" + "#{email}"
+		      puts "telefono:" + "#{phone}"
+		      puts "aux:" + "#{aux}"
+		      
+		      
+				  binding = RForce::Binding.new \
+     'https://www.salesforce.com/services/Soap/u/20.0'
+
+   				binding.login \
+     'admin@afd.co.cr', 'company12gzo99WBuTeLiA9cVY2lKUfNl0'
+   
+     			cliente = [
+                    :type,      'Candidato__c',
+                    :name,      "#{name}",
+                    :Apellidos__c,      "#{lname}",
+                    :Correo_Electronico__c,      "#{email}",
+                    :Telefono__c,      "#{phone}",
+                    :Descripcion__c,      "#{aux}",
+                    :Empresa_c__c, "AlternativaCR"
                    ]
 
      			binding.create :sObject => cliente		      
